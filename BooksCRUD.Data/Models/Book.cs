947204d2dabae4cdcd8f452932bb7380eae39707
shoebimtiaz <shoebimtiaz@gmail.com>
2025-12-10 10:36:19 -0600

    added image in models

--- BooksCRUD.Data/Models/Book.cs
+++ BooksCRUD.Data/Models/Book.cs
@@ -1,21 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BooksCRUD.Data.Models
+﻿namespace BooksCRUD.Data.Models
 {
     public class Book
     {
         public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        [Required]
-        public string Author { get; set; }
-        [Required]
-        public string Publisher { get; set; }
+        public string Name { get; set; } = default!;
+        public string Author { get; set; } = default!;
+        public string Publisher { get; set; } = default!;
 
+        public string? ImageUrl { get; set; }  // Store blob URL
     }
 }
---
+++ BooksCRUD.Web/Models/BookCreateViewModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+namespace BooksCRUD.Web.Models
+{
+    public class BookCreateViewModel
+    {
+        [Required]
+        public string Name { get; set; } = default!;
+
+        [Required]
+        public string Author { get; set; } = default!;
+
+        [Required]
+        public string Publisher { get; set; } = default!;
+
+        public IFormFile? Image { get; set; }
+    }
+}
